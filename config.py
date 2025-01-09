93a191717ac83ee40bfa016e847baae77e3a0d29
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-01-10 03:02:35 +0530

    a

---
+++ config.py
@@ -0,0 +1,19 @@
+TOKEN = ''
+
+WAIFU_TOKEN = ''
+
+WEBHOOK = (0, '')
+
+TOPGG = ''
+
+DATABASE_CRED = {
+    'user': '',
+    'password': '',
+    'database': '',
+    'host': '',
+    'port': 5432,
+}
+
+DEFAULT_PREFIX = 'm.'
+
+OWNERS_ID = []
