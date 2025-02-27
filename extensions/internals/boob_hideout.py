e1e56c8746205529106c4295745576d9291c98d2
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-02-28 00:50:56 +0530

    Remove wvc
    Signed-off-by: Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>

--- extensions/internals/boob_hideout.py
+++ extensions/internals/boob_hideout.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from discord.ext import commands
-
 from utils import BaseCog
 
 if TYPE_CHECKING:
@@ -14,14 +12,4 @@ def is_boob_hideout(ctx: Context) -> bool:
     return bool(ctx.guild and ctx.guild.id == 774561547930304536)
 
 
-class BoobHideout(BaseCog):
-    @commands.command('bhvc', help='Get members who are in vc in boob hideout')
-    @commands.check(is_boob_hideout)
-    async def wvc(self, ctx: Context) -> None:
-        guild = self.bot.get_guild(774561547930304536)
-        if not guild:
-            return
-        text = '\n'.join(
-            [f'## {ch.name}\n{",".join([str(mem) for mem in ch.members])}\n' for ch in guild.voice_channels if ch.members],
-        )
-        await ctx.reply(text)
+class BoobHideout(BaseCog): ...
