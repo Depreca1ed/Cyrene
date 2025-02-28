25d269b0864feebb9edf659c4082431a40336242
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-03-01 03:48:29 +0530

    Add ftp command
    Signed-off-by: Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>

--- extensions/internals/__init__.py
+++ extensions/internals/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from io import BytesIO
 from typing import TYPE_CHECKING, Any, Literal
+
+from jishaku.paginators import WrappedFilePaginator, PaginatorInterface
 
 if TYPE_CHECKING:
     from utils import Context, Mafuyu
@@ -104,6 +107,24 @@ class Internals(Blacklist, Developer, ErrorHandler, BoobHideout, name='Developer
     async def guild_leave(self, guild: discord.Guild) -> None:
         embed = guild_embed(guild, 'Left')
         await self.bot.logger.send(embed=embed)
+
+    @commands.command(name='file-to-pages', help='Turns a file into a pages to browse through', aliases=['ftp'])
+    async def ftp(self, ctx: Context, attachment: discord.Attachment | None) -> discord.Message | PaginatorInterface:
+        if not attachment and ctx.message.reference and isinstance(ctx.message.reference.resolved, discord.Message):
+            attachment = ctx.message.reference.resolved.attachments[0]
+
+        if not attachment:
+            raise commands.MissingRequiredArgument(commands.parameter(displayed_name='attachment'))
+
+        if attachment.size > 1024 * 1024 * 10:
+            return await ctx.send('File larged than 10MB')
+
+        if attachment.content_type and not attachment.content_type.startswith('text'):
+            return await ctx.send('Not a text document')
+
+        paginator = WrappedFilePaginator(BytesIO(await attachment.read()), max_size=1980)
+        interface = PaginatorInterface(self.bot, paginator)
+        return await interface.send_to(ctx)
 
 
 async def setup(bot: Mafuyu) -> None:
