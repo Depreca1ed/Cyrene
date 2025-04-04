37f8220f83a24ad0c875f42b360e34a1328e4700
Dep <70801324+Depreca1ed@users.noreply.github.com>
2025-04-05 02:14:34 +0530

    Update anicord_gacha.py

--- extensions/misc/anicord_gacha.py
+++ extensions/misc/anicord_gacha.py
@@ -458,7 +458,9 @@ class AniCordGacha(BaseCog):
             FROM
                 GachaPulledCards
             WHERE
-                user_id = $1;
+                user_id = $1
+           ORDER BY
+                rarity;
             """,
             user.id,
         )
