c15d9f67f05a1741e4970dcb105a9afc13c2d4f5
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-08-26 18:30:46 +0530

    Add back avatar command
    Signed-off-by: Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>

--- extensions/meta/userinfo.py
+++ extensions/meta/userinfo.py
@@ -81,3 +81,55 @@ class Userinfo(MafuCog):
         msg = await ctx.reply(embed=embed, view=view)
         if view:
             view.message = msg
+
+    @commands.hybrid_command(
+        name='avatar',
+        description="Get your or user's displayed avatar. By default, returns your server avatar",
+        aliases=['av'],
+    )
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @app_commands.allowed_installs(guilds=True, users=True)
+    async def avatar(
+        self,
+        ctx: MafuContext,
+        user: discord.User | discord.Member = commands.Author,
+        *,
+        server_avatar: bool = True,
+    ) -> discord.Message:
+        avatar = user.display_avatar if server_avatar is True else user.avatar or user.default_avatar
+
+        embed = Embed(title=f"{user}'s avatar", colour=user.color).set_image(url=avatar.url)
+
+        filetypes = set(discord.asset.VALID_ASSET_FORMATS if avatar.is_animated() else discord.asset.VALID_STATIC_FORMATS)
+        urls_string = fmt_str(
+            [f'[{filetype.upper()}]({avatar.with_format(filetype)})' for filetype in filetypes],  # pyright: ignore[reportArgumentType]
+            seperator=' **|** ',
+        )
+        embed.description = urls_string
+
+        return await ctx.send(embed=embed)
+
+    @commands.hybrid_command(name='icon', description="Get the server's icon, if any")
+    @app_commands.allowed_contexts(guilds=True, dms=False, private_channels=False)
+    @app_commands.allowed_installs(guilds=True, users=False)
+    @commands.guild_only()
+    async def guild_avatar(self, ctx: MafuContext) -> discord.Message:
+        if not ctx.guild:
+            msg = 'Guild not found'
+            raise commands.GuildNotFound(msg)
+
+        icon = ctx.guild.icon
+        if not icon:
+            return await ctx.reply(content=f'{commands.clean_content().convert(ctx, str(ctx.guild))} does not have an icon.')
+
+        embed = Embed(title=f"{ctx.guild}'s icon").set_image(url=icon.url)
+
+        filetypes = set(discord.asset.VALID_ASSET_FORMATS if icon.is_animated() else discord.asset.VALID_STATIC_FORMATS)
+
+        urls_string = fmt_str(
+            [f'[{filetype.upper()}]({icon.with_format(filetype)})' for filetype in filetypes],  # pyright: ignore[reportArgumentType]
+            seperator=' **|** ',
+        )
+        embed.description = urls_string
+
+        return await ctx.send(embed=embed)
