bc273a6879e465d4bfc71d2ab69c998308e122e5
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-10-31 21:01:32 +0530

    ,

--- __main__.py
+++ __main__.py
@@ -1,12 +1,3 @@
-"""
-This file was sourced from [RoboDanny](https://github.com/Rapptz/RoboDanny).
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""
-# This copy of launcher.py has been modified very deeply but still sources back to RoboDanny's
-
 from __future__ import annotations
 
 import asyncio
