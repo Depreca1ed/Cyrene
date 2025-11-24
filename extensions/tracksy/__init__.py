1265fdb38b60d5536896248f1f3fc7d33250ed60
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-11-25 00:47:40 +0530

    Kill me

--- extensions/tracksy/__init__.py
+++ extensions/tracksy/__init__.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from extensions.tracksy.frontend import Frontend
 from extensions.tracksy.tracker import Tracker
 
 if TYPE_CHECKING:
     from utilities.bases.bot import Cyrene
 
 
-class Tracksy(Tracker, name='Tracksy'): ...
+class Tracksy(Tracker, Frontend, name='Tracksy'): ...
 
 
 async def setup(bot: Cyrene) -> None:
