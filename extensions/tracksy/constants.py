ccd255cbb1e3a80681c6b95716293583fe8bd583
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-11-24 23:06:30 +0530

    Add ignore regex for "NEW!"

--- extensions/tracksy/constants.py
+++ extensions/tracksy/constants.py
@@ -5,7 +5,7 @@ import discord
 ANICORD_DISCORD_BOT = 1257717266355851384
 ANICORD_GACHA_SERVER = 1242232552845086782
 
-PULLALL_LINE_REGEX = r'Name: `(?P<name>.+)` Rarity: <:(?P<rarity>[a-zA-Z0-9]+):.+>.+ ID: `(?P<id>[0-9]+)`'
+PULLALL_LINE_REGEX = r'Name: `(?P<name>.+)` Rarity: <:(?P<rarity>[a-zA-Z0-9]+):.+>.+ ID: `(?P<id>[0-9]+)`*'
 
 SINGLE_PULL_REGEX = r"""Rarity: <:(?P<rarity>[a-zA-Z0-9]+):.+>
 Burn Worth: (?P<burn_worth>[0-9]+)
